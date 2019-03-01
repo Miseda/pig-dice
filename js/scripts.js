af0dbe3d3301fb28e46132c4420ce8f60a8069d1
Miseda <misedaalex@gmail.com>
2019-03-01 16:15:23 +0300

    Added functionality to the button

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,8 @@
-function rolldice() {
-    var p1=document.getElementById("p1-score");
-    var c1=document.getElementById("c0-score");
-    var player1=Math.floor(Math.random() *6 +1);
-    p1.innerHTML=player1;
-    if(player1=== 0){
-        $(".btn1").hide()
-        $(".btn2").show()
-    }
-function roll() {
-    var p2=document.getElementById("p2-score");
-    var c2=document.getElementById("c1-score");
-    var player2=Math.floor(Math.random() *6 +1);
-    p2.innerHTML=player2;
-    if(player2===0){
-        $(".btn2").hide
-        $(".btn1").show
-    }
+function dice(){
+    return Math.floor(Math.random() *6) +1
 }
-}
-
+   $(document).ready(function() {
+   $("button#roll").click(function(){
+    $("#p1-score").text(dice())
+    })
+   })
