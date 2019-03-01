8809f05bfa2baa99217061d514edb54978c72149
Miseda <misedaalex@gmail.com>
2019-03-01 14:10:15 +0300

    Added an image to the app.

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function rolldice() {
+    var p1=document.getElementById("p1-score");
+    var p2=document.getElementById("p2-score");
+    var c1=document.getElementById("c0-score");
+    var c2=document.getElementById("c1-score");
+}
