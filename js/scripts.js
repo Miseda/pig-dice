60fc4511daf280c933498d47c055b5e0ff8ebb17
Miseda <misedaalex@gmail.com>
2019-03-01 15:50:01 +0300

    Added another button and seperated the two players.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 function rolldice() {
     var p1=document.getElementById("p1-score");
-    var p2=document.getElementById("p2-score");
     var c1=document.getElementById("c0-score");
-    var c2=document.getElementById("c1-score");
     var player1=Math.floor(Math.random() *6 +1);
+    p1.innerHTML=player1;
+    if(player1=== 0){
+        $(".btn1").hide()
+        $(".btn2").show()
+    }
+function roll() {
+    var p2=document.getElementById("p2-score");
+    var c2=document.getElementById("c1-score");
+    var player2=Math.floor(Math.random() *6 +1);
+    p2.innerHTML=player2;
+    if(player2===0){
+        $(".btn2").hide
+        $(".btn1").show
+    }
 }
+}
+
