214f6adda6ae1b87ced4e9b34ff931f3c7598f57
Miseda <misedaalex@gmail.com>
2019-03-01 17:16:58 +0300

    Added player 2 to the game .

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
-function dice(){
-    return Math.floor(Math.random() *6) +1
+ function dice(){
+    return Math.floor(Math.random() *6) +1;
 }
    $(document).ready(function() {
-   $("button#roll").click(function(){
-    $("#p1-score").text(dice())
+   $("#roll").click(function(){
+    var num = dice();
+    if(num == 1){
+        $(".btn2").show();
+        $(".btn1").hide();
+        
+    }
+    $("#p1-score").text(num)
+
+    $("#rollDice").click(function(){
+    var num = dice();
+    if(num == 1){
+        $(".btn1").show();
+        $(".btn2").hide();
+    }
+    $("#p2-score").text(num)
+    
+    })
+    
     })
    })
