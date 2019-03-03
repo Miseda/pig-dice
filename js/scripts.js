98bbcd3b01906a15c116291576e3af3976f72b49
Miseda <misedaalex@gmail.com>
2019-03-03 03:12:51 +0300

    Added a hold button.

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,20 @@
    $("#roll").click(function(){
     var num1 = dice();
     $("#p1-score").text(num1)
+    $("#c0-score").text(total)
+    var total= 0;
+    var num1A= [dice()];
+
     if(num1 == 1){
         $(".btn2").show();
         $(".btn1").hide();
-        
+    }else{
+      num1A.forEach(function(num){
+        total += num;
+      });
     }
 })
-    
+
 
     $("#rollDice").click(function(){
     var num2 = dice();
@@ -20,9 +27,23 @@
         $(".btn1").show();
         $(".btn2").hide();
     }
-    
-    
     })
-    
-    
+
+    document.getElementById("hold")
+    $("#hold").click(function(){
+      $(".btn1").hide();
+      $(".btn2").show();
+    })
+    var p1Name = $("input#player1name").val();
+    var p2Name = $("input#player2name").val();
+    $("#entry").click(function() {
+      $(".game").show();
+      $(".opening").hide();
+      $("#p1-name").text(p1Name);
+      $("#p2-name").text(p2Name);
+    });
+
+
+
+
    })
